3bc41b95701be95dc1619b6a2ea49e6834030893
Abdul Majid <abdulmajidcse@gmail.com>
2023-02-06 11:00:45 +0600

    add action field and export csv

--- app/Http/Livewire/Auth/Category/CategoryDataTable.php
+++ app/Http/Livewire/Auth/Category/CategoryDataTable.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Livewire\Auth\Category;
 
-use App\Exports\CategoriesExport;
 use App\Models\Category;
+use App\Exports\CategoriesExport;
 use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Database\Eloquent\Builder;
+use Maatwebsite\Excel\Excel as ExcelExcel;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Columns\LinkColumn;
+use Rappasoft\LaravelLivewireTables\Views\Columns\ButtonGroupColumn;
 
 class CategoryDataTable extends DataTableComponent
 {
@@ -17,17 +20,29 @@ class CategoryDataTable extends DataTableComponent
     public function bulkActions(): array
     {
         return [
-            'export' => 'Export',
+            'exportExcel' => 'Export Excel',
+            'exportCsv' => 'Export CSV',
         ];
     }
 
-    public function export()
+    public function exportExcel()
     {
         $categories = $this->getSelected();
 
         $this->clearSelected();
 
-        return Excel::download(new CategoriesExport($categories), 'all-categories.xlsx');
+        return Excel::download(new CategoriesExport($categories), 'all-categories.xlsx', ExcelExcel::XLSX);
+    }
+
+    public function exportCsv()
+    {
+        $categories = $this->getSelected();
+
+        $this->clearSelected();
+
+        return Excel::download(new CategoriesExport($categories), 'all-categories.csv', ExcelExcel::CSV, [
+            'Content-Type' => 'text/csv',
+        ]);
     }
 
     public function configure(): void
@@ -46,15 +61,38 @@ class CategoryDataTable extends DataTableComponent
         return [
             Column::make('SL')
                 ->label(fn ($row) => ++$this->sl),
-            Column::make('ID', 'id')->sortable()->searchable(),
+            Column::make('ID', 'id')->sortable()->searchable()->deselected(),
             Column::make("Name", "name")
                 ->sortable()->searchable(),
             Column::make("Slug", "slug")->sortable()->searchable(),
             Column::make('Parent', 'parent_id')
                 ->format(fn ($value, $row) => $row->parent?->name ?: '...')
                 ->sortable()->searchable(),
-            Column::make("Created Date", "created_at")
-                ->sortable()->searchable(),
+            ButtonGroupColumn::make('Actions')
+                ->attributes(function ($row) {
+                    return [
+                        'class' => 'space-x-2',
+                    ];
+                })
+                ->buttons([
+                    LinkColumn::make('View') // make() has no effect in this case but needs to be set anyway
+                        ->title(fn ($row) => 'View')
+                        ->location(fn ($row) => route('auth.categories.edit', $row))
+                        ->attributes(function ($row) {
+                            return [
+                                'class' => 'underline text-blue-500 hover:no-underline',
+                            ];
+                        }),
+                    LinkColumn::make('Edit')
+                        ->title(fn ($row) => 'Edit')
+                        ->location(fn ($row) => route('auth.categories.edit', $row))
+                        ->attributes(function ($row) {
+                            return [
+                                'target' => '_blank',
+                                'class' => 'underline text-blue-500 hover:no-underline',
+                            ];
+                        }),
+                ]),
         ];
     }
 
